Gammaloop integrands can now properly specify the integrand, or sum over all integrands in a group. Implemented discrete sampling over graphs in x-space. Fixed a bug that caused time-variance scaling observables to be incorrect.

src/glnis/core/accumulator.py
@@ -101,6 +101,7 @@ class IntegrationResult:
         self.abs_imag_error = ce(self.abs_imag_error, other.abs_imag_error)
 
         self.n_points += other.n_points
+        self.total_time += other.total_time
         self.__post_init__()  # Recalculate derived observables
 
     def str_report(self: 'IntegrationResult') -> str:
@@ -873,4 +874,4 @@ class TrainingData(AccumulatorModule):
         sqr_n_points = np.sqrt(self.training_result[0].shape[0])
         self.central_value = np.mean(self.training_result[0])
         self.error = np.std(self.training_result[0]) / sqr_n_points
-        self.rsd = self.error / self.central_value * sqr_n_points if self.central_value != 0 else 0
+        self.rsd = np.abs(self.error / self.central_value) * sqr_n_points if self.central_value != 0 else 0

src/glnis/core/integrand.py
@@ -133,6 +133,8 @@ class GammaLoopIntegrand(Integrand):
 
     def __init__(self,
                  gammaloop_state_path: str,
+                 process_id: int = 0,
+                 integrand_name: str = "",
                  run_commands: str | List[str] = "",
                  momentum_space: bool = True,
                  sample_lmbs: bool = False,
@@ -142,7 +144,7 @@ class GammaLoopIntegrand(Integrand):
                  read_only_state: bool = True,
                  **kwargs):
         try:
-            from gammaloop import GammaLoopAPI
+            import gammaloop
         except:
             raise ImportError("Failed to import gammaloop module.")
         super().__init__(**kwargs)
@@ -150,52 +152,84 @@ class GammaLoopIntegrand(Integrand):
         self.use_arb_prec = use_arb_prec
         self.minimal_output = minimal_output
 
-        self.gammaloop_state = GammaLoopAPI(gammaloop_state_path, read_only_state=read_only_state)
+        self.gammaloop_state = gammaloop.GammaLoopAPI(
+            gammaloop_state_path,
+            level=gammaloop.LogLevel.Off,
+            logfile_level=gammaloop.LogLevel.Off,
+            read_only_state=read_only_state)
         if run_commands:
             if isinstance(run_commands, str):
                 run_commands = [run_commands]
             for cmd in run_commands:
                 self.gammaloop_state.run(cmd)
-        outputs = [o for o in self.gammaloop_state.list_outputs() if len(0) > 0][0]
-        # Just select the first one, kek
-        self.integrand_name = list(outputs.keys())[0]
-        self.process_id = outputs[self.integrand_name]
-        state_info = self.gammaloop_state.get_integrand_info(self.process_id, self.integrand_name)
-        # TODO: validate the approaches to getting info about discrete sampling from gl integrand, also which comes first
-        # Check whether we need to provide discrete input to gammaloop
-        self.sample_graphs = len(state_info.graph_groups) > 1
+        self.outputs = [o for o in self.gammaloop_state.list_outputs() if len(o) > 0][process_id]
+        if integrand_name not in self.outputs or integrand_name == "summed":
+            integrand_name = list(self.outputs)[0]
+            process_id = self.outputs[integrand_name]
+        else:
+            process_id = self.outputs[integrand_name]
+            self.outputs = {integrand_name: process_id}
+        self.sample_graphs = len(self.graph_properties) > 1
+        if self.sample_graphs and self.momentum_space:
+            raise ValueError("Discrete sampling over graphs is not supported in momentum space.")
         self.discrete_dims = []
+        self._discrete_types: List[str] = []
+        self._gl_discrete_types: Dict[str, int] = dict(graph=0)
         if self.sample_graphs:
-            self.discrete_dims.append(len(state_info.graph_groups))
+            self.discrete_dims.append(len(self.graph_properties))
+            self._discrete_types.append("graph")
         if sample_orientations:
-            self.gammaloop_state.run("set default-runtime kv sampling.orientations 'monte_carlo'")
-            n_orientations = [gp.n_orientations for gp in self.graph_properties]
-            self.discrete_dims.append(max(n_orientations))
+            for itg_name, pid in self.outputs.items():
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.orientations='monte_carlo'")
+            n_orientations = max([gp.n_orientations for gp in self.graph_properties])
+            if n_orientations > 1:
+                self.discrete_dims.append(n_orientations)
+                self._discrete_types.append("orientation")
+            self._gl_discrete_types["orientation"] = len(self._gl_discrete_types)
         else:
-            self.gammaloop_state.run("set default-runtime kv sampling.orientations 'summed'")
+            for itg_name, pid in self.outputs.items():
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.orientations='summed'")
         if sample_lmbs:
-            self.gammaloop_state.run("set default-runtime kv sampling.lmb_multichanneling true")
-            # Need to make sure the LMB heuristics are not overwritten
-            n_channels = [gp.n_channels for gp in self.graph_properties]
-            self.discrete_dims.append(max(n_channels))
+            for itg_name, pid in self.outputs.items():
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.lmb_multichanneling=true")
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.lmb_channels='monte_carlo'")
+            n_channels = max([gp.n_channels for gp in self.graph_properties])
+            self._mask_lmb_dims = n_channels == 1
+            if not self._mask_lmb_dims:
+                self.discrete_dims.append(n_channels)
+                self._discrete_types.append("lmb_channel")
+            self._gl_discrete_types["lmb_channel"] = len(self._gl_discrete_types)
         else:
-            self.gammaloop_state.run("set default-runtime kv sampling.lmb_multichanneling false")
+            for itg_name, pid in self.outputs.items():
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.lmb_multichanneling=false")
+                self.gammaloop_state.run(
+                    f"set process -p {pid} -i {itg_name} kv sampling.lmb_channels='summed'")
 
     def _evaluate_batch(self, continuous: NDArray, discrete: NDArray) -> NDArray:
         # When not sampling graphs, prepend graph_id=0 for gammaloop
-        discrete_dims = np.hstack([
-            np.zeros((len(continuous), int(not self.sample_graphs)), dtype=np.uint64),
-            discrete,
-        ])
-        res = self.gammaloop_state.evaluate_samples(
-            points=continuous.astype(np.float64), discrete_dims=discrete_dims,
-            momentum_space=self.momentum_space,
-            process_id=self.process_id,
-            integrand_name=self.integrand_name,
-            use_arb_prec=self.use_arb_prec,
-            minimal_output=self.minimal_output,
-        )
-        numpy_res = np.array([s.integrand_result for s in res.samples])
+        discrete_dims = np.zeros((len(continuous), len(self._gl_discrete_types)), dtype=np.uint64)
+        for dim, tpe in enumerate(self._discrete_types):
+            gl_dim = self._gl_discrete_types[tpe]
+            discrete_dims[:, gl_dim] = discrete[:, dim]
+        numpy_res = np.zeros(len(continuous), dtype=np.complex128)
+        for itg_name, pid in self.outputs.items():
+            res = self.gammaloop_state.evaluate_samples(
+                points=continuous.astype(np.float64), discrete_dims=discrete_dims,
+                momentum_space=self.momentum_space,
+                process_id=pid,
+                integrand_name=itg_name,
+                use_arb_prec=self.use_arb_prec,
+                minimal_output=self.minimal_output,
+            )
+            itg_res = np.array([s.integrand_result for s in res.samples])
+            if not self.momentum_space:
+                itg_res *= np.array([s.parameterization_jacobian for s in res.samples])
+            numpy_res += itg_res
         return numpy_res.reshape(-1, 1)
 
     def discrete_prior_prob_function(self, indices: NDArray, _: int = 0) -> NDArray:
@@ -209,25 +243,24 @@ class GammaLoopIntegrand(Integrand):
         if num_disc_input == len(self.discrete_dims):
             return np.zeros_like(indices, dtype=np.float64)
 
-        match num_disc_input:
-            case 0:
-                n_graphs = self.discrete_dims[0]
+        disc_type = self._discrete_types[num_disc_input]
+        match disc_type:
+            case "graph":
+                n_graphs = self.discrete_dims[num_disc_input]
                 prior = np.ones((len(indices), n_graphs), dtype=np.float64)
-            case 1:
-                max_orientations = self.discrete_dims[1]
+            case "orientation":
+                max_orientations = self.discrete_dims[num_disc_input]
                 n_or_arr = np.array([gp.n_orientations for gp in self.graph_properties])
                 n_per_graph = n_or_arr[indices[:, 0]]
                 prior = (np.arange(max_orientations) < n_per_graph[:, None]).astype(np.float64)
-            case 2:
-                max_channels = self.discrete_dims[2]
+            case "lmb_channel":
+                max_channels = self.discrete_dims[num_disc_input]
                 n_ch_arr = np.array([gp.n_channels for gp in self.graph_properties])
                 n_per_graph = n_ch_arr[indices[:, 0]]
                 prior = (np.arange(max_channels) < n_per_graph[:, None]).astype(np.float64)
-            case len(self.discrete_dims):
-                return np.zeros_like(indices, dtype=np.float64)
             case _:
                 raise ValueError(
-                    f"Invalid number of discrete input dimensions: {num_disc_input}. Expected at most {len(self.discrete_dims)}.")
+                    f"Invalid type of discrete input dimensions: {disc_type}. Expected one of {self._discrete_types}.")
 
         return prior / np.prod(prior, axis=1, keepdims=True)
 

src/glnis/scripts/multiprocessing_efficiency.py
@@ -45,8 +45,13 @@ def run_multiprocessing_efficiency(
     signal.signal(signal.SIGINT, signal.default_int_handler)
 
     file = verify_path(file)
-    with file.open('rb') as f:
-        SData = torch.load(f, weights_only=False)
+    try:
+        with file.open('rb') as f:
+            SData = torch.load(f, weights_only=False)
+    except:
+        with file.open('rb') as f:
+            SData = pickle.load(f)
+
     if isinstance(SData, SamplerCompData):
         pass
     elif isinstance(SData, MPEfficiencyData):
@@ -64,9 +69,9 @@ def run_multiprocessing_efficiency(
         # parameters
         scripts: Dict[str, Any] = Settings.settings.get("scripts", dict())
         params: Dict[str, Any] = scripts.get(subroutine, dict())
-        n_cores: List[int] = params.get("n_cores", [1, 2, 4, 8, 16, 32, 64, 128])
-        n_samples: List[int] = params.get("n_samples", [1_000, 10_000, 100_000, 1_000_000, 10_000_000])
-        max_samples_per_core: int = params.get("max_samples_per_core", 100_000)
+        n_cores: List[int] = params.get("n_cores", [1, 2, 4, 8, 16, 32, 64, 128, 256])
+        n_samples: List[int] = params.get("n_samples", [1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000])
+        max_samples_per_core: int = params.get("max_samples_per_core", 1_000_000)
 
         madnis_state = SData.integrator_states.get("MadNIS", None)
 
@@ -102,7 +107,7 @@ def run_multiprocessing_efficiency(
             for samples in n_samples:
                 if samples / cores > max_samples_per_core:
                     continue
-                acc = madnis_integrator.integrate(samples)
+                acc = madnis_integrator.integrate(samples, n_start=10_000_000)
                 times = MPEfficiencyData.Times(cores, samples, acc.processing_times.processing_times)
                 Data.data.append(times)
 
@@ -125,7 +130,10 @@ def run_multiprocessing_efficiency(
         madnis_integrator.free()
     except Exception as e:
         shell_print(f"\nCaught Exception — stopping workers: {e}")
+        from traceback import print_exc
+        print_exc()
         madnis_integrator.free()
+        raise
     finally:
         madnis_integrator.free()
 
@@ -179,6 +187,7 @@ def plot_multiprocessing_efficiency(file: str, comment: str = "") -> None:
 
     ax.set_xlabel("Number of Samples")
     ax.set_ylabel(r"$t_{eval}$ [CPU-µs]")
+    ax.set_yscale("log")
     ax.legend(title=r"$n_{cores}$", loc="upper right")
     ax.set_title("Total time")
     fig.savefig(
@@ -219,6 +228,7 @@ def plot_multiprocessing_efficiency(file: str, comment: str = "") -> None:
 
         ax.set_xlabel("Number of Samples")
         ax.set_ylabel(r"$t_{eval}$ [CPU-µs]")
+        ax.set_yscale("log")
         ax.legend(title=r"$n_{cores}$", loc="upper right")
         match name:
             case "sampler":

src/glnis/scripts/sampler_comparison.py
@@ -72,13 +72,39 @@ def run_sampler_comp(
     no_plot: bool = False,
     export_states: bool = True,
     subroutine: str = "sampler_comp",
+    overwrite_plotting_settings: str = "",
 ) -> SamplerCompData | None:
+
+    plotting_settings = dict()
+    if overwrite_plotting_settings:
+        try:
+            import tomllib
+            with open(verify_path(overwrite_plotting_settings, suffix=".toml"), "rb") as f:
+                _overwrite = tomllib.load(f)
+            plotting_settings = _overwrite.get(subroutine, dict()).get("plotting", dict())
+            if len(plotting_settings) == 0:
+                plotting_settings = _overwrite.get("sampler_comp", dict()).get("plotting", dict())
+        except:
+            pass
 
     if isinstance(file, dict):
         pass
     elif Path(file).suffix == ".pkl":
-        plot_sampler_comp(file, comment)
-        return
+        from torch import load as torch_load
+        with Path(file).open('rb') as f:
+            SData = torch_load(f, weights_only=False)
+        if len(plotting_settings) == 0:
+            scripts = SData.settings.get("scripts", dict())
+            params = scripts.get(subroutine, dict())
+            if len(params) == 0:
+                params = scripts.get("sampler_comp", dict())
+            plotting_settings = params.get("plotting", dict())
+        if isinstance(SData, SamplerCompData):
+            plot_sampler_comp(file, SData, comment, plotting_settings)
+            return
+        else:
+            raise ValueError(
+                f"Expected a SamplerCompData object in the file, but got {type(SData)}")
 
     import signal
     import gc
@@ -116,6 +142,8 @@ def run_sampler_comp(
         # Training parameters
         scripts: Dict[str, Any] = Settings.settings.get("scripts", dict())
         params: Dict[str, Any] = scripts.get(subroutine, dict())
+        if len(params) == 0:
+            params = scripts.get("sampler_comp", dict())
         n_training_steps = params.get("n_training_steps", 1000)
         n_log = params.get("n_log", 10)
         n_plot_rsd = params.get("n_plot_rsd", 100)
@@ -124,8 +152,9 @@ def run_sampler_comp(
         n_plot_disc = params.get("n_plot_disc", 10)
         n_samples = params.get("n_samples", 10_000)
         n_samples_after_training = params.get("n_samples_after_training", 100_000)
-        nitn = params.get('nitn', 10)  # number of vegas/havana training iterations
-        # number of evals per vegas/havana training iteration
+        nitn = params.get("nitn", 10)  # number of vegas/havana training iterations
+        if len(plotting_settings) == 0:
+            plotting_settings = params.get("plotting", dict())
 
         if not no_output:
             PROJECT_ROOT = Path(__file__).parents[3]
@@ -265,7 +294,7 @@ def run_sampler_comp(
             torch.save(Data, f)
 
         if not no_plot:
-            plot_sampler_comp(file, comment)
+            plot_sampler_comp(file, Data, comment, plotting_settings)
 
         return Data
 
@@ -282,16 +311,16 @@ def run_sampler_comp(
         end_all_integrators()
 
 
-def plot_sampler_comp(file: str, comment: str = "") -> None:
+def plot_sampler_comp(file: str,
+                      Data: SamplerCompData,
+                      comment: str = "",
+                      plotting_settings: Dict[str, Any] = dict()) -> None:
     import matplotlib.pyplot as plt
     import matplotlib.colors as colors
-    from torch import load as torch_load
 
     file: Path = verify_path(file, suffix=".pkl")
-    with file.open('rb') as f:
-        Data: SamplerCompData = torch_load(f, weights_only=False)
-        if not isinstance(Data, SamplerCompData):
-            raise ValueError(f"Expected a SamplerCompData object in the file, but got {type(Data)}")
+    if not isinstance(Data, SamplerCompData):
+        raise ValueError(f"Expected a SamplerCompData object in the file, but got {type(Data)}")
 
     shell_print(f"Plotting data from '{file}'")
 
@@ -431,10 +460,12 @@ def plot_sampler_comp(file: str, comment: str = "") -> None:
         )
 
     if discrete_probs is not None:
-        n_show = 3
+        n_show = plotting_settings.get("n_show_best_worst_discrete", 3)
         sorted_indices = np.argsort(discrete_probs[-1])[::-1]
         if sorted_indices.size > 2*n_show:
-            show_indices = sorted_indices[np.r_[0:n_show, -n_show:0]]
+            show_indices = sorted_indices[
+                np.r_[0:n_show, -n_show:0]
+            ]
         else:
             show_indices = sorted_indices
         show_probs = discrete_probs.T[show_indices]